df52aa2cddf87d7a6d1b40c66c4d3cfb74db7932
martinhiriart <mhiriart@duck.com>
2023-10-10 12:14:52 -0500

    Added check for Windows self-assigned IPs

--- main.go
+++ main.go
@@ -28,12 +28,12 @@ func main() {
 	}
 	var v4IPs []string
 	for _, ipAddr := range ifIPs {
-		if !strings.Contains(ipAddr.String(), "::") && !strings.Contains(ipAddr.String(), ":") && !strings.Contains(ipAddr.String(), "127.0.0.1") {
+		if !strings.Contains(ipAddr.String(), "::") && !strings.Contains(ipAddr.String(), ":") && !strings.Contains(ipAddr.String(), "127.0.0.1") && !strings.Contains(ipAddr.String(), "169.254") {
 			v4IPs = append(v4IPs, ipAddr.String())
 		}
 	}
 
-	v4IPStr := strings.Join(v4IPs, ";\n\t\t\t\t")
+	v4IPStr := strings.Join(v4IPs, ";\n\t\t\t\t ")
 
 	hostname, err := os.Hostname()
 	usrInfo, err := user.LookupId(userInfo.Uid)
@@ -41,12 +41,12 @@ func main() {
 		panic(err)
 	}
 
-	hostnameStr := fmt.Sprintf("Hostname:\t\t%v\n", hostname)
-	osStr := fmt.Sprintf("OS:\t\t\t\t%v %v\n", hostInfo.OS.Name, hostInfo.OS.Version)
-	archStr := fmt.Sprintf("Architecture:\t\t%v\n", hostInfo.Architecture)
-	tmzStr := fmt.Sprintf("Time Zone:\t\t%v\n", hostInfo.Timezone)
-	ipStr := fmt.Sprintf("IP Addresses:\t\t%v\n", v4IPStr)
-	usrStr := fmt.Sprintf("Current User:\t\t%v\n", usrInfo.Username)
+	hostnameStr := fmt.Sprintf("Hostname:\t\t %v\n", hostname)
+	osStr := fmt.Sprintf("Operating System: %v %v\n", hostInfo.OS.Name, hostInfo.OS.Version)
+	archStr := fmt.Sprintf("Architecture:\t\t %v\n", hostInfo.Architecture)
+	tmzStr := fmt.Sprintf("Time Zone:\t\t %v\n", hostInfo.Timezone)
+	ipStr := fmt.Sprintf("IP Addresses:\t\t %v\n", v4IPStr)
+	usrStr := fmt.Sprintf("Current User:\t\t %v\n", usrInfo.Username)
 
 	err = zenity.Info(hostnameStr+usrStr+osStr+archStr+tmzStr+ipStr,
 		zenity.Title("System Information"),
