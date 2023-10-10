5c2b59d0d86a2e5e9a81ec49e0ba74d1ca206f94
martinhiriart <mhiriart@duck.com>
2023-10-10 18:17:55 -0500

    Organized logic into smaller chunks and created internal styling package

---
+++ internal/styling/output_styling.go
@@ -0,0 +1,13 @@
+package styling
+
+import "strings"
+
+func FormatString(stringsToFormat []string) string {
+	outputString := strings.Join(stringsToFormat, "\n")
+	return outputString
+}
+
+func FormatIPStrings(IPs []string) string {
+	IPstring := strings.Join(IPs, ";\n\t\t\t\t ")
+	return IPstring
+}
--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"github.com/elastic/go-sysinfo"
+	"github.com/elastic/go-sysinfo/types"
+	"github.com/martinhiriart/sysinfo/internal/styling"
 	"github.com/ncruces/zenity"
 	"net"
 	"os"
@@ -10,22 +12,44 @@ import (
 	"strings"
 )
 
-func main() {
+func getHostInfo() types.HostInfo {
 	host, err := sysinfo.Host()
 	if err != nil {
 		panic(err)
 	}
+	return host.Info()
+}
 
-	ifIPs, err := net.InterfaceAddrs()
+func getIPAddresses() []net.Addr {
+	interfIPs, err := net.InterfaceAddrs()
 	if err != nil {
 		panic(err)
 	}
+	return interfIPs
+}
 
-	hostInfo := host.Info()
+func getCurrentUser() *user.User {
 	userInfo, err := user.Current()
 	if err != nil {
 		panic(err)
 	}
+	return userInfo
+}
+
+func main() {
+
+	hostInfo := getHostInfo()
+	ifIPs := getIPAddresses()
+	userInfo := getCurrentUser()
+	hostname, err := os.Hostname()
+	if err != nil {
+		panic(err)
+	}
+	usrInfo, err := user.LookupId(userInfo.Uid)
+	if err != nil {
+		panic(err)
+	}
+
 	var v4IPs []string
 	for _, ipAddr := range ifIPs {
 		if !strings.Contains(ipAddr.String(), "::") && !strings.Contains(ipAddr.String(), ":") && !strings.Contains(ipAddr.String(), "127.0.0.1") && !strings.Contains(ipAddr.String(), "169.254") {
@@ -33,26 +57,32 @@ func main() {
 		}
 	}
 
-	v4IPStr := strings.Join(v4IPs, ";\n\t\t\t\t ")
+	v4IPStr := styling.FormatIPStrings(v4IPs)
 
-	hostname, err := os.Hostname()
-	usrInfo, err := user.LookupId(userInfo.Uid)
-	if err != nil {
-		panic(err)
-	}
+	var stringsToOutput []string
 
-	hostnameStr := fmt.Sprintf("Hostname:\t\t %v\n", hostname)
-	osStr := fmt.Sprintf("Operating System: %v %v\n", hostInfo.OS.Name, hostInfo.OS.Version)
-	archStr := fmt.Sprintf("Architecture:\t\t %v\n", hostInfo.Architecture)
-	tmzStr := fmt.Sprintf("Time Zone:\t\t %v\n", hostInfo.Timezone)
-	ipStr := fmt.Sprintf("IP Addresses:\t\t %v\n", v4IPStr)
-	usrStr := fmt.Sprintf("Current User:\t\t %v\n", usrInfo.Username)
+	hostnameStr := fmt.Sprintf("Hostname:\t\t %v", hostname)
+	osStr := fmt.Sprintf("Operating System: %v %v", hostInfo.OS.Name, hostInfo.OS.Version)
+	archStr := fmt.Sprintf("Architecture:\t\t %v", hostInfo.Architecture)
+	tmzStr := fmt.Sprintf("Time Zone:\t\t %v", hostInfo.Timezone)
+	ipStr := fmt.Sprintf("IP Addresses:\t\t %v", v4IPStr)
+	usrStr := fmt.Sprintf("Current User:\t\t %v", usrInfo.Username)
 
-	err = zenity.Info(hostnameStr+usrStr+osStr+archStr+tmzStr+ipStr,
+	stringsToOutput = append(stringsToOutput, hostnameStr)
+	stringsToOutput = append(stringsToOutput, osStr)
+	stringsToOutput = append(stringsToOutput, archStr)
+	stringsToOutput = append(stringsToOutput, tmzStr)
+	stringsToOutput = append(stringsToOutput, ipStr)
+	stringsToOutput = append(stringsToOutput, usrStr)
+
+	outputString := styling.FormatString(stringsToOutput)
+
+	err = zenity.Info(outputString,
 		zenity.Title("System Information"),
-		zenity.Icon("internal/diagIcon.png"))
+		zenity.Icon("internal/assets/diagIcon.png"))
 
 	if err != nil {
 		panic(err)
 	}
+
 }
